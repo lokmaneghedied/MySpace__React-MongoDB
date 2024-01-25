6f339a9c1876bcd3827779e0316b8a7855e33244
lokmaneghedied <lokmane.ghedied@gmail.com>
2024-01-25 17:33:25 +0100

    update description

--- js/src/pages/Home.js
+++ js/src/pages/Home.js
@@ -1,15 +1,17 @@
 import LogIn from "../components/LogIn";
 
 const Home = () => {
-    return ( 
-        <div className="bg-blue-100 h-screen grid justify-items-center md:flex md:justify-around md:items-center ">
-            <div className="font-serif">
-                <h1 className="text-blue-500 md:text-6xl text-4xl font-medium">MySpace</h1>
-                <p className="md:text-2xl pt-3">Take this opportunity to express</p>
-            </div>
-            <LogIn />
-        </div>
-     );
-}
- 
+  return (
+    <div className="bg-blue-100 h-screen grid justify-items-center md:flex md:justify-around md:items-center ">
+      <div className="font-serif">
+        <h1 className="text-blue-500 md:text-6xl text-4xl font-medium">
+          MySpace
+        </h1>
+        <p className="md:text-2xl pt-3">new description</p>
+      </div>
+      <LogIn />
+    </div>
+  );
+};
+
 export default Home;
